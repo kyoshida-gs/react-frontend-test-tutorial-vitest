2d8529643a50fa3746ce12be8da4eb6ec15e7d76
Kazutaka Yoshida <kazutaka.yoshida@g-seed.co.jp>
2026-03-04 18:17:49 +0900

    shadcn/ui の設定

--- src/components/input/TextInput.tsx
+++ src/components/input/TextInput.tsx
@@ -1,19 +1,31 @@
 import { useState } from 'react';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
 
 const TextInput = () => {
     const [text, setText] = useState<string>('');
 
     return (
-        <div>
-            <input
-                type="text"
-                onChange={(e) => setText(e.target.value)}
-                value={text}
-                placeholder="Enter text"
-                className="border-2 border-gray-300 rounded-md p-2"
-            />
+        <>
+            <div className="flex flex-row gap-1 items-center">
+                {/* <input
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        placeholder="Enter text"
+        className="border-2 border-gray-300 rounded-md p-2"
+      /> */}
+                <Input
+                    type="text"
+                    onChange={(e) => setText(e.target.value)}
+                    value={text}
+                    placeholder="Enter text"
+                />
+
+                <Button>Click me</Button>
+            </div>
             <p>{text}</p>
-        </div>
+        </>
     );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
     test: {
         environment: 'happy-dom',
         globals: true,
