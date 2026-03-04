d3ac8ea8534276de911ed55acab6f2fa830dd771
Kazutaka Yoshida <kazutaka.yoshida@g-seed.co.jp>
2026-03-04 14:34:39 +0900

    テストコード

---
+++ src/components/input/TextInput.test.tsx
@@ -0,0 +1,20 @@
+// import "@testing-library/jest-dom/vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TextInput from "./TextInput";
+
+test("TextInput Component Text", async () => {
+  render(<TextInput />);
+
+  const inputElement = screen.getByRole("textbox");
+  expect(inputElement).toBeInTheDocument();
+});
+
+test("TextInput Event Text", async () => {
+  const user = userEvent.setup();
+  render(<TextInput />);
+
+  const inputElement = screen.getByRole("textbox");
+  await user.type(inputElement, "Hello, world!");
+  expect(screen.getByText("Hello, world!")).toBeInTheDocument();
+});
---
+++ src/components/input/TextInput.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const TextInput = () => {
+  const [text, setText] = useState<string>("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        placeholder="Enter text"
+      />
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default TextInput;
