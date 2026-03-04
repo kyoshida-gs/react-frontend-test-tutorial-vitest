19b7231c56d6dc0018accadae1bdcb7379c0da20
Kazutaka Yoshida <kazutaka.yoshida@g-seed.co.jp>
2026-03-04 17:57:50 +0900

    TailwindCSS の設定

--- src/components/input/TextInput.tsx
+++ src/components/input/TextInput.tsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const TextInput = () => {
-  const [text, setText] = useState<string>("");
+    const [text, setText] = useState<string>('');
 
-  return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => setText(e.target.value)}
-        value={text}
-        placeholder="Enter text"
-      />
-      <p>{text}</p>
-    </div>
-  );
+    return (
+        <div>
+            <input
+                type="text"
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+                placeholder="Enter text"
+                className="border-2 border-gray-300 rounded-md p-2"
+            />
+            <p>{text}</p>
+        </div>
+    );
 };
 
 export default TextInput;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    environment: "happy-dom",
-    globals: true,
-    setupFiles: ["./vitest-setup.ts"],
-  },
+    plugins: [react(), tsconfigPaths(), tailwindcss()],
+    test: {
+        environment: 'happy-dom',
+        globals: true,
+        setupFiles: ['./vitest-setup.ts'],
+    },
 });
