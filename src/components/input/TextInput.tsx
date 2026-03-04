2836a299d4a03c2bd184e8ff4f5ca614c43a4328
Kazutaka Yoshida <kazutaka.yoshida@g-seed.co.jp>
2026-03-04 18:56:50 +0900

    Storybook

--- src/components/input/TextInput.tsx
+++ src/components/input/TextInput.tsx
@@ -9,12 +9,12 @@ const TextInput = () => {
         <>
             <div className="flex flex-row gap-1 items-center">
                 {/* <input
-        type="text"
-        onChange={(e) => setText(e.target.value)}
-        value={text}
-        placeholder="Enter text"
-        className="border-2 border-gray-300 rounded-md p-2"
-      /> */}
+                    type="text"
+                    onChange={(e) => setText(e.target.value)}
+                    value={text}
+                    placeholder="Enter text"
+                    className="border-2 border-gray-300 rounded-md p-2"
+                /> */}
                 <Input
                     type="text"
                     onChange={(e) => setText(e.target.value)}
@@ -22,7 +22,7 @@ const TextInput = () => {
                     placeholder="Enter text"
                 />
 
-                <Button>Click me</Button>
+                {<Button>Click me</Button>}
             </div>
             <p>{text}</p>
         </>
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,49 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths(), tailwindcss()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+    setupFiles: ['./vitest-setup.ts'],
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
         },
-    },
-    test: {
-        environment: 'happy-dom',
-        globals: true,
-        setupFiles: ['./vitest-setup.ts'],
-    },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
 });
